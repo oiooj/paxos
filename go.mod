3f5c053a46629d8f037efdbe62fae0a38116f3e5
kun <oiooj@qq.com>
2020-05-06 20:12:24 +0800

    add test case

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/oiooj/paxos
 
-go 1.15
+go 1.12
--- paxos_test.go
+++ paxos_test.go
@@ -85,3 +85,34 @@ func TestPaxosWithTwoProposers2(t *testing.T) {
 	}
 	time.Sleep(time.Millisecond)
 }
+
+func TestPaxosWithTwoProposers3(t *testing.T) {
+	// 1, 2, 3 are acceptors
+	// 1001,1002 is a proposer
+	pn := newPaxosNetwork(1, 2, 3, 1001, 1002, 2001)
+
+	as := make([]*acceptor, 0)
+	for i := 1; i <= 3; i++ {
+		as = append(as, newAcceptor(i, pn.agentNetwork(i), 2001))
+	}
+
+	for _, a := range as {
+		go a.run()
+	}
+
+	p11 := newProposer(1002, "hello world", pn.agentNetwork(1002), 1, 2, 3)
+	p12 := newProposer(1002, "hello world2", pn.agentNetwork(1002), 1, 2, 3)
+	go p11.run()
+	go p12.run()
+
+	p2 := newProposer(1001, "bad day", pn.agentNetwork(1001), 1, 2, 3)
+	go p2.run()
+
+	l := newLearner(2001, pn.agentNetwork(2001), 1, 2, 3)
+	value := l.learn()
+	// only one value will be chosen on greater server ID.
+	if value == "bad day" {
+		t.Errorf("value = %s, want %s", value, "hello world or hello world2")
+	}
+	time.Sleep(time.Millisecond)
+}
